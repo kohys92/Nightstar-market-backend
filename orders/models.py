a73aa62dfa130b4f7895ae0126f87a2b56f55f08
Chi heon Song <hrpp1353@gmail.com>
2021-09-01 15:48:26 +0900

    Modified : order_item_status modified

--- orders/models.py
+++ orders/models.py
@@ -9,16 +9,16 @@ class Order(models.Model):
         db_table = 'orders'
 
 class OrderItem(models.Model):
-    product  = models.ForeignKey('products.Product', on_delete = models.CASCADE)
-    order    = models.ForeignKey('Order', on_delete = models.CASCADE)
-    quantity = models.IntegerField()
+    product           = models.ForeignKey('products.Product', on_delete = models.CASCADE)
+    order             = models.ForeignKey('Order', on_delete = models.CASCADE)
+    quantity          = models.IntegerField()
+    order_item_status = models.ForeignKey('OrderItemStatus', null = True, on_delete = models.CASCADE)
 
     class Meta:
         db_table = 'order_items'
 
 class OrderItemStatus(models.Model):
-    order_item        = models.ForeignKey('OrderItem', on_delete = models.CASCADE)
-    order_item_status = models.CharField(max_length = 45)
+    name = models.CharField(max_length = 45)
 
     class Meta:
         db_table = 'order_statuses'
