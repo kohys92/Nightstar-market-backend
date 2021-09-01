4042a52d02946a88883bc14f470fb317cc696914
Chi heon Song <hrpp1353@gmail.com>
2021-09-01 17:06:18 +0900

    Add2 : Cart in products model

--- orders/views.py
+++ orders/views.py
@@ -1,19 +1,2 @@
-import json
+from django.shortcuts import render
 
-from django.http  import JsonResponse
-from django.views import View
-
-from products import Product
-from users    import User
-
-class CartView(View):
-    data = json.loads(request.body)
-
-    product_id       = data['product_id']
-    user_id           = data['user_id']
-    purchase_quantity = data['quantity']
-
-    product = Product.objects.get(id = products_id)
-    user    = User.objects.get(id = products_id)
-
-    
