1b6a1c83cc0f8ad1a2510c380ead2ce1e687546e
Young Soo Koh <kohys92@gmail.com>
2021-09-03 19:24:55 +0900

    Modify: urls added

--- bbmarket/urls.py
+++ bbmarket/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('bbmarket', include('products.urls')),
     path('bbmarket', include('users.urls')),
+    path('bbmarket', include('products.urls')),
 ]
