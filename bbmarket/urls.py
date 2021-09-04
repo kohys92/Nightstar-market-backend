bf78cf0713a4c0ae676ed73371114934a2d68b9f
Young Soo Koh <kohys92@gmail.com>
2021-09-04 12:13:25 +0900

    Modify : urls.py modified and POST method removed from productView

--- bbmarket/urls.py
+++ bbmarket/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    # path('bbmarket', include('users.urls')),
-    path('bbmarket', include('products.urls')),
+    path('products', include('products.urls')),
 ]
