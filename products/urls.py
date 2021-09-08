f80ac80eba9a3e19a652b507382c8131cb6be08b
김정수 <jungsookim@gimjeongsuui-MacBookAir.local>
2021-09-08 17:45:09 +0900

    Add & Modify : add DetailViewr and modify SignupView's date_of_birth

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductViewer
+from .views import ProductViewer, DetailViewer
 
 urlpatterns = [
     path('/lists', ProductViewer.as_view()),
+    path('/<int:id>', DetailViewer.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
+from random import randint
+
 from django.views import View
 from django.http import JsonResponse
+from django.db.models import Max, Min
 
-from .models import Product 
+from .models import Product
 
 class ProductViewer(View):  
     def get(self,request):
@@ -13,6 +16,7 @@ class ProductViewer(View):
             
             return JsonResponse({'products' : [
                 {
+                    'id'            : product.id,
                     'name'          : product.name,
                     'price'         : product.price,
                     'discount'      : product.discount,
@@ -33,3 +37,56 @@ class ProductViewer(View):
             
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+
+class DetailViewer(View):
+    def get(self, request, id):
+        try:
+            if not Product.objects.filter(id = id).exists():
+                return JsonResponse({'MESSAGE' : 'NOT FOUND'}, status = 404)
+            
+            product = Product.objects.get(id = id)
+
+            main_id = product.sub_category.main_category_id
+            related_products = Product.objects.filter(sub_category__main_category_id = main_id)
+
+            max_id = related_products.aggregate(max_id = Max('id'))
+            min_id = related_products.aggregate(min_id = Min('id'))
+
+            selected_products = []
+            while len(selected_products) < 11:
+                current_product = related_products.get(id = randint(min_id['min_id'], max_id['max_id']))
+                if current_product in selected_products:
+                    continue
+                selected_products.append({
+                    "image_url" : current_product.productimage_set.first().image_url,
+                    "price"     : current_product.price,
+                    "name"      : current_product.name
+                })
+        
+            return JsonResponse(
+                {
+                    'id'            : product.id,
+                    'name'          : product.name,
+                    'price'         : product.price,
+                    'discount'      : product.discount,
+                    'sales_unit'    : product.sales_unit,
+                    'weight'        : product.weight,
+                    'shipping_type' : product.shipping_type,
+                    'origin'        : product.origin,
+                    'package_type'  : product.package_type,
+                    'infomation'    : product.infomation,
+                    'created_at'    : product.created_at,
+                    'updated_at'    : product.updated_at,
+                    'sub_category'  : product.sub_category.name,
+                    'main_category' : product.sub_category.main_category.name,
+                    'menu'          : product.sub_category.main_category.menu.name,
+                    'image_list'    : [img.image_url for img in product.productimage_set.all()],
+                    'allergy_list'  : [allergies.name for allergies in product.allergy.all()],
+                    'selected_products' : selected_products,
+                }, status = 200
+            )
+
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status = 400)
+
+
