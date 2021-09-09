66c4a79979dea8eef15b239de1d22b8b1ac2eb19
Chi heon Song <hrpp1353@gmail.com>
2021-09-09 16:07:08 +0900

    Modified2 : add query parameter

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,6 @@
-from orders.views import CartView
 from django.urls  import path
 from .views       import CartView
 
 urlpatterns = [
     path('/carts', CartView.as_view()),
-    path('/carts/{product_id}?', CartView.as_view())
 ]
