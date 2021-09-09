1bd9210664ffbdfebafc4669ea888c3118fbef47
김정수 <jungsookim@gimjeongsuui-MacBookAir.local>
2021-09-09 14:56:58 +0900

    Modify : ID duplication function added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import SignUpView, LogInView
+from users.views import SignUpView, LogInView, DuplicationView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/login', LogInView.as_view()), 
+    path('/signup/duplication', DuplicationView.as_view()),
 ]
